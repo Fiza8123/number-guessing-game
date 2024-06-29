431237fa70b4363f54cdfba886a46c47cb340a6a
Fiza8123 <159928018+Fiza8123@users.noreply.github.com>
2024-06-29 13:00:37 +0500

    Add files via upload

---
+++ number.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+let guess = Math.floor(Math.random()  *10+1)
+
+let guessnumber = await inquirer.prompt(
+    
+         {
+            message :"enter yuor number between 1-10:",
+            type : "number",
+            name : "yourguess",
+         }
+    
+);
+
+if(guessnumber.yourguess === guess){
+    console.log("congratulation  !!! your guess is right ........");
+}else{
+    console.log("opp's your guess is wrong");
+}
